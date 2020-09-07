1ea4999835614d26947e830c97a68e646aade231
Blake Davis <blakedavis@Blakes-MacBook-Pro.local>
2020-09-06 20:38:36 -0400

    Added initial dependencies, created database table, and general project layout completed

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/users.db3',
+  },
+  migrations: {
+    directory: './migrations',
+  },
+  seeds: {
+    directory: './seeds',
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done);
+    },
+  },
+};
